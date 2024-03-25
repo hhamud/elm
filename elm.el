8aa418708dd4b42ca7f70e5f48d87a1fe8d057fa
User <53880692+hhamud@users.noreply.github.com>
2024-03-25 01:00:43 +0000

    feat: fix response parsing

--- elm.el
+++ elm.el
@@ -21,39 +21,44 @@
 
 (require 'request)
 
-(defconst elm--claude-key "")
+(defconst elm--claude-key "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")
 
 (defvar elm--header
     `(("x-api-key" . ,elm--claude-key)
       ("anthropic-version" . "2023-06-01")
-      ("content-type" . "application/json")))
+      ("Content-Type" . "application/json")))
 
 (defconst elm--url "https://api.anthropic.com/v1/messages")
 
 (defun elm--construct-content (content)
-  "Contruct the content to send to claude."
+  "Contruct the CONTENT to send to claude."
   `(("model" . "claude-3-opus-20240229")
-    ("max_tokens" . "1024")
-    ("message" . (("role" . "user")
-                   ("content" . ,content)))))
+    ("max_tokens" . 1024)
+    ("messages" . ((("role" . "user")
+                   ("content" . ,content))))))
 
 (defun elm-send-request (data)
-  "send the DATA request to CLAUDE."
+  "Send the DATA request to CLAUDE."
   (request elm--url
     :type "POST"
-    :header elm--header
+    :headers elm--header
     :data (json-encode (elm--construct-content data))
     :parser 'json-read
     :success (cl-function
                 (lambda (&key data &allow-other-keys)
-                  (message "Response :%S" data)))
+                  (let* (
+                        (resp-text (cdr (assoc 'content data)))
+                        (final-resp (cdr (assoc 'text (aref resp-text 0)))))
+                    (message "Response :%s" final-resp))))))
     :error (cl-function
             (lambda (&key response &allow-other-keys)
               (let* ((error-data (request-response-data response))
                      (error-type (cdr (assoc 'type (cdr (assoc 'error error-data)))))
                      (error-message (cdr (assoc 'message (cdr (assoc 'error error-data))))))
-                (message "Error: %s -%s" error-type error-message))))))
+                (message "Error: %s -%s" error-type error-message))))
 
 
+
+
+(elm-send-request "testing")
 (provide 'elm)
-;;; elm.el ends here
